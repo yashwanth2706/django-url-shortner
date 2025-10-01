7a670f022cd9c66db35fba66377975fe457df8e5
Yashwanth A <yash2706.git@gmail.com>
2025-10-01 05:40:20 +0000

    Added inline edit for short and long URL

--- url_shortner/urls.py
+++ url_shortner/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('recent-urls/', views.recent_urls, name='recent_urls'),
+    path("edit-short-url/<int:pk>/", views.edit_short_url, name="edit_short_url"),
+    path("edit-long-url/<int:pk>/", views.edit_long_url, name="edit_long_url"),
     # This pattern should be last so it doesn't catch other paths like /admin
     path('<str:short_code>/', views.redirect_view, name='redirect'),
 ]
